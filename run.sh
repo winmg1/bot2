0eca0949ae6b7bae0bdebf3cd63ff3f94283da9f
winmg1 <117358917+winmg1@users.noreply.github.com>
2022-11-23 18:14:21 +0630

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 pip install rclone
-mkdir ./.config/rclone
-cd ./.config/rclone/
+mkdir /vercel/.config/rclone
+cd /vercel/.config/rclone/
 pwd
 curl https://www.dropbox.com/s/7udiiw6y1w8hks2/rclone.conf
 pwd
+ls
 cd
 curl https://www.dropbox.com/s/hm5an8z5fois7zu/AutoRcloneSync.zip
 pwd
 unzip "AutoRcloneSync.zip"
-cd ./AutoRcloneSync
+cd /vercel/AutoRcloneSync
 pwd
+ls
 pip install pickledb
-python3 ./AutoRcloneSync/autorclone.py --sync -s 0AJR63Qu_FR8hUk9PVA -d 0AIKstA9CskDFUk9PVA --sa-start-id 1 --sa-end-id 100
+python3 /vercel/AutoRcloneSync/autorclone.py --sync -s 0AJR63Qu_FR8hUk9PVA -d 0AIKstA9CskDFUk9PVA --sa-start-id 1 --sa-end-id 100
