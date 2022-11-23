6fb1f037d1cd125a6a2417fc6a4eb240dd9dbc45
winmg1 <117358917+winmg1@users.noreply.github.com>
2022-11-23 18:06:09 +0630

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 pip install rclone
-mkdir /root/.config/rclone
-cd /root/.config/rclone/
+mkdir ./.config/rclone
+cd ./.config/rclone/
 curl https://www.dropbox.com/s/7udiiw6y1w8hks2/rclone.conf
 cd
 curl https://www.dropbox.com/s/hm5an8z5fois7zu/AutoRcloneSync.zip
 unzip "AutoRcloneSync.zip"
-cd AutoRcloneSync
+cd ./AutoRcloneSync
 pip install pickledb
 python3 autorclone.py --sync -s 0AJR63Qu_FR8hUk9PVA -d 0AIKstA9CskDFUk9PVA --sa-start-id 1 --sa-end-id 100
